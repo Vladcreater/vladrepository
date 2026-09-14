6a5427b4d76973e86b031e14c51311d8d2f7b928
Vlad <6estfr1nd@gmail.com>
2026-09-15 01:48:53 +0300

    Fix indentation in calc_disc function

--- topic1/TP-KB-252-Vlad-Baklazhko/topic_01/task3.py
+++ topic1/TP-KB-252-Vlad-Baklazhko/topic_01/task3.py
@@ -1,5 +1,5 @@
 def calc_disc(num_a, num_b, num_c):
-return (num_b ** 2) - (4 * num_a * num_c)
+    return (num_b ** 2) - (4 * num_a * num_c)
 coeff_a = float(input("Введіть a: "))
 coeff_b = float(input("Введіть b: "))
 coeff_c = float(input("Введіть c: "))
